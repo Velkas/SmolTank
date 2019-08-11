3da242bdc8089a5ca9a95477c6a9ba239f740d42
Velkas <treestump34@gmail.com>
2019-08-10 23:56:26 -0700

    Initial Environment layout
    WebGL target
    Minor tweaks to colliders  and materials

---
+++ src/Assets/GlobalInteraction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalInteraction : MonoBehaviour
+{
+    void Update()
+    {
+        Application.Quit();
+    }
+}
